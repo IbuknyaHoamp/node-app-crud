842a30c0e0fb0378ea337457b91a548a84039470
IbuknyaHoamp <sukarnis243@gmail.com>
2023-01-10 07:59:42 +0700

    fitur create

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-req
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+// use express
+const app = express()
+
+// konfigurasi database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_product'
+})
+
+// menjalankan mysql
+conn.connect((err) => {
+    if(err) throw err
+    console.log('Connected to mysql')
+})
+
+// setting views
+app.set('views', path.join(__dirname,'views'))
+// setting view engine
+app.set('view engine', 'hbs')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// homepage route
+app.get('/', (req, res) => {
+    let sql = "SELECT * FROM product"
+    let query = conn.query(sql, (err, results) =>{
+        if(err) throw err
+        res.render('product_view', {
+            results: results
+        })
+    })
+})
+
+// route insert data
+app.get('/create', (req, res) => {
+    res.render('product_create')
+})
+app.post('/save', (req, res) => {
+    let data = {
+        name: req.body.name, 
+        price: req.body.price
+    }
+    let sql = "INSERT INTO product SET ?"
+    let query = conn.query(sql, data, (err, results) => {
+        if(err) throw err
+        res.redirect('/')
+    })
+})
+
+// server
+app.listen(8000, () => {
+    console.log('Server is running at port 8000');
+});
